a4074df72db5a48794f36b786666d8498af416a5
Peter McHale <peter.thomas.mchale@gmail.com>
2017-09-19 14:16:26 -0700

    minor

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-nohup caffeinate -i nice -19 python ./src/train.py < /dev/null > ./data/train.dat 2>&1 &
+nohup caffeinate -i nice -19 python ./src/train.py < /dev/null > ./data/train.dat 2>./data/train.log &
